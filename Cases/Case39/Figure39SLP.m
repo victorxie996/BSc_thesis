49c734dfc4b126fa9faf80a5b45492067c8afcb7
Victor Xie <56447006+victorxie996@users.noreply.github.com>
2020-10-30 16:59:34 +0000

    Add files via upload

---
+++ Cases/Case39/Figure39SLP.m
@@ -0,0 +1,17 @@
+load('iteration.mat')
+
+
+subplot(3,1,1);
+plot(iteration(:,1),iteration(:,2),'b')
+xlabel('iterations'),ylabel('trust region'), title('progress of trust region')
+
+% the constraint ratio against iteration plot does not mean alot.
+% plot(iteration(:,1),iteration(:,8))
+% xlabel('iterations'),ylabel('constraint ratio'), title('case 9')
+% figure;
+subplot(3,1,2);
+plot(iteration(:,1),iteration(:,5),'r')
+xlabel('iterations'),ylabel('objective value'), title('progress of objective value')
+subplot(3,1,3);
+plot(iteration(:,1),iteration(:,6),'r')
+xlabel('iterations'),ylabel('constraint violation'), title('progress of constraint violation')
